4b2d512517169d234f6c85116d7738e634083788
Zhaohui <zhangzh@xmpaoyou.com>
2019-08-01 14:22:05 +0800

    在导出前先删除。

--- publish.cli.bat
+++ publish.cli.bat
@@ -3,6 +3,8 @@ echo ===============================
 echo «Î…‘µ», excel to client lua
 echo ===============================
 
+del /Q %~dp0..\mnClient\Assets\mnRes\Lua\Data\sdata\*.lua
+
 set PATH=%PATH%;%~dp0..\mnSrv\www\php7
 
 php %~dp0..\mnSrv\admin\excel_to_lua.php
--- publish.srv.bat
+++ publish.srv.bat
@@ -3,6 +3,8 @@ echo ===============================
 echo «Î…‘µ», excel to server php
 echo ===============================
 
+del /Q %~dp0..\mnSrv\common\static_data\*.php
+
 set PATH=%PATH%;%~dp0..\mnSrv\www\php7
 
 php %~dp0..\mnSrv\admin\excel_to_php.php
